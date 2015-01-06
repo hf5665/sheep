0c1d18fd4b5e2ebe6282eaeffcb8ff2602158bbf
hf5665 <hf5665@gmail.com>
2015-01-06 18:34:25 +0800

    generate jni header file

--- AndroidJNIProject/jni/NativeMath.cpp
+++ AndroidJNIProject/jni/NativeMath.cpp
@@ -1,1 +1,11 @@
 #include <jni.h>
+#include <stdio.h>
+#include "com_example_androidjniproject_MainActivity.h"
+
+JNIEXPORT jstring JNICALL Java_org_magiclen_androidjni_MainActivity_helloString(
+		JNIEnv* env, jobject obj, jstring str){
+	const char* toWhat = env->GetStringUTFChars(str, JNI_FALSE);
+	char hello[80];
+	sprintf(hello,"Hello, %s!", toWhat);
+	return env->NewStringUTF(hello);
+} 
